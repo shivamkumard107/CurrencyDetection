c81e72fc047a2df4312e5b942e971920c8cc06c7
shivamkumar107 <shivam.kumard107@gmail.com>
2020-03-28 23:42:56 +0530

    permissions added

--- app/src/main/java/gad/heartbeat/androidflask/easyupload/AskPermissionActivity.java
+++ app/src/main/java/gad/heartbeat/androidflask/easyupload/AskPermissionActivity.java
@@ -1,18 +1,32 @@
 package gad.heartbeat.androidflask.easyupload;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.Vibrator;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AskPermissionActivity extends AppCompatActivity {
     private MediaPlayer mPlayer_en, mPlayer_hi;
     private Button btSkip;
     private Vibrator mVib;
+    private static final int REQUEST_CAMERA = 1;
+    private static final int REQUEST_VIBRATE = 2;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 3;
+    private static final int REQUEST_AUDIO = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +44,21 @@ public class AskPermissionActivity extends AppCompatActivity {
                 mPlayer_en.stop();
                 mPlayer_hi.stop();
                 mVib.vibrate(50);
-                //TODO: check and ask for permission here then proceed to next activity
-
-                SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putBoolean("permission", true);
-                editor.apply();
-                startActivity(new Intent(AskPermissionActivity.this, InstructionActivity.class));
-                finish();
+                if (checkAndRequestPermissions()) {
+                    SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean("permission", true);
+                    editor.apply();
+                    startActivity(new Intent(AskPermissionActivity.this, InstructionActivity.class));
+                    finish();
+                }
             }
         });
     }
-    public void audioPlayer(){
+
+    public void audioPlayer() {
         //set up MediaPlayer
-        mPlayer_en= MediaPlayer.create(AskPermissionActivity.this, R.raw.permission);
+        mPlayer_en = MediaPlayer.create(AskPermissionActivity.this, R.raw.permission);
         mPlayer_hi = MediaPlayer.create(AskPermissionActivity.this, R.raw.permission_hi);
         mPlayer_en.start();
         mPlayer_en.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -56,14 +71,44 @@ public class AskPermissionActivity extends AppCompatActivity {
         mPlayer_hi.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPref.edit();
-                editor.putBoolean("permission", true);
-                editor.apply();
-                startActivity(new Intent(AskPermissionActivity.this, InstructionActivity.class));
-                finish();
+                if (checkAndRequestPermissions()) {
+                    SharedPreferences sharedPref = getSharedPreferences("mySettings", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putBoolean("permission", true);
+                    editor.apply();
+                    startActivity(new Intent(AskPermissionActivity.this, InstructionActivity.class));
+                    finish();
+                }
             }
         });
 
     }
+
+    private boolean checkAndRequestPermissions() {
+        int permissionStorage = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        int audioPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (audioPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.RECORD_AUDIO);
+        }
+
+        if (permissionStorage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 1);
+            return false;
+        }
+        return true;
+    }
+
 }
